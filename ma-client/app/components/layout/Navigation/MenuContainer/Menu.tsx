6e53e7615ae445d99a94a425305e8ca6d962df42
IDDna <romanov1371@gmail.com>
2023-05-01 20:45:57 +0300

    Fix bugs with server and client sides

--- ma-client/app/components/layout/Navigation/MenuContainer/Menu.tsx
+++ ma-client/app/components/layout/Navigation/MenuContainer/Menu.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic'
 import { FC } from 'react'
 
 import styles from './Menu.module.scss'
@@ -5,15 +6,19 @@ import MenuItem from './MenuItem'
 import AuthItems from './auth/AuthItems'
 import { IMenu } from './menu.interface'
 
+const DynamicAuthItems = dynamic(() => import('./auth/AuthItems'), {
+  ssr: false,
+})
+
 const Menu: FC<{ menu: IMenu }> = ({ menu: { items, title } }) => {
   return (
     <div className={styles.menu}>
       <div className={styles.heading}>{title}</div>
       <ul className={styles.ul}>
-        {items.map((item) => (
+        {items.map(item => (
           <MenuItem key={item.link} item={item} />
         ))}
-        {title === 'General' ? <AuthItems /> : null}
+        {title === 'General' ? <DynamicAuthItems /> : null}
       </ul>
     </div>
   )
