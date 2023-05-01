4c28bfa8f15cf2206ab2d4487e3590d84be26c00
IDDna <romanov1371@gmail.com>
2023-05-01 18:11:10 +0300

    Implement big slider on Home page

--- ma-client/pages/index.tsx
+++ ma-client/pages/index.tsx
@@ -1,9 +1,50 @@
-import Home from '@/components/screens/home/Home'
+import { errorCatch } from 'api/api.helpers'
+import { GetStaticProps, NextPage } from 'next'
 
-export default function HomePage() {
+import Home from '@/components/screens/home/Home'
+import { IHome } from '@/components/screens/home/home.interface'
+import { ISlide } from '@/components/ui/slider/slider.interface'
+
+import { MovieService } from '@/services/movie.service'
+
+import { getGenresList } from '@/utils/movie/getGenresList'
+
+import { getMovieUrl } from '@/config/url.config'
+
+const HomePage: NextPage<IHome> = ({ slides }) => {
   return (
     <div>
-      <Home />
+      <Home slides={slides} />
     </div>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const { data: movies } = await MovieService.getAll()
+
+    const slides: ISlide[] = movies.slice(0, 3).map(m => ({
+      _id: m._id,
+      link: getMovieUrl(m.slug),
+      subTitle: getGenresList(m.genres),
+      title: m.title,
+      bigPoster: m.bigPoster,
+    }))
+
+    return {
+      props: {
+        slides,
+      } as IHome,
+    }
+  } catch (error) {
+    console.log(errorCatch(error))
+
+    return {
+      props: {
+        slides: [],
+      } as IHome,
+    }
+  }
+}
+
+export default HomePage
