8d71d61a97c7580336882954b5c050d7789986d8
IDDna <romanov1371@gmail.com>
2023-05-14 14:04:19 +0300

    Write fetch file

--- ma-client/src/api/fetch.ts
+++ ma-client/src/api/fetch.ts
@@ -1,1 +1,15 @@
-export const 
+import axios from './interceptors'
+
+type TypeInput = {
+  url: string
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  body?: any
+}
+
+export const api = async ({ url, method, body }: TypeInput) => {
+  return axios({
+    method,
+    url,
+    data: body,
+  })
+}
